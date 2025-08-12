4f25f88de42dc4a063df2e0dd05148335ed68bb8
jupitters <sampaiobale@gmail.com>
2025-08-12 15:49:08 -0300

    trailer path added to App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import {Route, Routes} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
 
 function App() {
   const [movies, setMovies] = useState();
@@ -29,7 +30,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path='/' element={<Home movies={movies}/>}></Route>
+          <Route path='/' element={<Home movies={movies}/>} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer/>} />
 
         </Route>
       </Routes>
