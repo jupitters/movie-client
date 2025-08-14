17fa3fdfe0e1fb0563b0935fcc371b4a15c41e17
jupitters <sampaiobale@gmail.com>
2025-08-13 23:48:38 -0300

    fixed function

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -6,12 +6,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap';
-import Reviews from '../reviews/Reviews';
 
 const Hero = ({movies = []}) => {
     const navigate = useNavigate();
 
-    function reviews() {
+    function reviews(movieId) {
         navigate(`/Reviews/${movieId}`);
     }
 
