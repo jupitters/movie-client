b314db4c7a9a9c5eae30e73c01a5925ea5b14b2b
jupitters <sampaiobale@gmail.com>
2025-08-13 23:49:20 -0300

    add in app

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         <Route path="/" element={<Layout/>}>
           <Route path='/' element={<Home movies={movies}/>} />
           <Route path="/Trailer/:ytTrailerId" element={<Trailer/>} />
-          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} reviews={reviews} setReviews={setReviews} />} />
+          <Route path="/Reviews/:movieId" element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />} />
 
         </Route>
       </Routes>
