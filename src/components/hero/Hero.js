f267dcdc5d187c4bb245ff40fcf010288a118dad
jupitters <sampaiobale@gmail.com>
2025-08-13 23:48:10 -0300

    fixed button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,7 +5,7 @@ import { Paper } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap';
 import Reviews from '../reviews/Reviews';
 
 const Hero = ({movies = []}) => {
